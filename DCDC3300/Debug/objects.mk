c126dd1485a062f4bff5fc159a397628b16d9ca4
hungchungyu <hungchungyu514@gmail.com>
2024-04-19 16:27:03 +0800

    Clear

--- DCDC3300/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"D:/TICode/C2000Ware/C2000Ware_5_01_00_00/driverlib/f28004x/driverlib/ccs/Debug/driverlib_eabi.lib"
-
